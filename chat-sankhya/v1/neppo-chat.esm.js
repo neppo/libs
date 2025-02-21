a19f186224f6d3e320e6c3d702446fefe6b919af
gustavofalleiros <gustavo.alves.falleiros@gmail.com>
2025-02-21 09:31:56 -0300

    chat sankhya

--- chat-sankhya/v1/neppo-chat.esm.js
+++ chat-sankhya/v1/neppo-chat.esm.js
@@ -1,2 +1,2 @@
-import{p as a,b as r}from"./p-f063a8d7.js";export{s as setNonce}from"./p-f063a8d7.js";import{g as t}from"./p-e1255160.js";var e=()=>{const r=import.meta.url;const s={};if(r!==""){s.resourcesUrl=new URL(".",r).href}return a(s)};e().then((async a=>{await t();return r([["p-48414e17",[[1,"neppo-chat",{parameters:[1]}]]]],a)}));
+import{p as a,b as r}from"./p-f063a8d7.js";export{s as setNonce}from"./p-f063a8d7.js";import{g as t}from"./p-e1255160.js";var p=()=>{const r=import.meta.url;const s={};if(r!==""){s.resourcesUrl=new URL(".",r).href}return a(s)};p().then((async a=>{await t();return r([["p-1c44f90c",[[1,"neppo-chat",{parameters:[1]}]]]],a)}));
 //# sourceMappingURL=neppo-chat.esm.js.map
